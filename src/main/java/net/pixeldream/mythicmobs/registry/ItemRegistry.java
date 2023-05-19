86e73a0b1d65cd692ca9d0481761d312a4274dcd
kyber <100732009+kyber-6@users.noreply.github.com>
2023-05-19 22:25:01 +0200

    Added bronze block crafting recipe

--- src/main/java/net/pixeldream/mythicmobs/registry/ItemRegistry.java
+++ src/main/java/net/pixeldream/mythicmobs/registry/ItemRegistry.java
@@ -15,11 +15,11 @@ public class ItemRegistry {
     public static final Item KOBOLD_WARRIOR_SPAWN_EGG = new SpawnEggItem(EntityRegistry.KOBOLD_WARRIOR_ENTITY,0xd5f07d, 0x637036, new FabricItemSettings().group(ITEM_GROUP).maxCount(64));
     public static final Item CHUPACABRA_SPAWN_EGG = new SpawnEggItem(EntityRegistry.CHUPACABRA_ENTITY,0x444541, 0x82261e, new FabricItemSettings().group(ITEM_GROUP).maxCount(64));
     public static final KoboldSpearItem KOBOLD_SPEAR = new KoboldSpearItem(ToolMaterials.IRON, 4, 1, new Item.Settings().group(ITEM_GROUP).maxCount(1));
-    public static final Item BRONZE_ITEM = new Item(new FabricItemSettings().group(ITEM_GROUP).maxCount(64));
+    public static final Item BRONZE_INGOT = new Item(new FabricItemSettings().group(ITEM_GROUP).maxCount(64));
     public static final BlockItem BRONZE_BLOCK = new BlockItem(BlockRegistry.BRONZE_BLOCK, new FabricItemSettings().group(ITEM_GROUP).maxCount(64));
     public ItemRegistry() {
         Registry.register(Registry.ITEM, new Identifier(MOD_ID, "kobold_spear"), KOBOLD_SPEAR);
-        Registry.register(Registry.ITEM, new Identifier(MOD_ID, "bronze_item"), BRONZE_ITEM);
+        Registry.register(Registry.ITEM, new Identifier(MOD_ID, "bronze_ingot"), BRONZE_INGOT);
         Registry.register(Registry.ITEM, new Identifier(MOD_ID, "bronze_block"),BRONZE_BLOCK);
         Registry.register(Registry.ITEM, new Identifier(MOD_ID, "automaton_spawn_egg"), AUTOMATON_SPAWN_EGG);
         Registry.register(Registry.ITEM, new Identifier(MOD_ID, "kobold_spawn_egg"), KOBOLD_SPAWN_EGG);
