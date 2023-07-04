afa92306c5f4080c079b4a547082a962b319958f
kyber-6 <100732009+kyber-6@users.noreply.github.com>
2023-07-04 14:01:49 +0200

    Implemented `onUse` method

---
+++ src/main/java/net/pixeldream/mythicmobs/block/BronzeBlock.java
@@ -0,0 +1,30 @@
+package net.pixeldream.mythicmobs.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.pixeldream.mythicmobs.registry.BlockRegistry;
+
+public class BronzeBlock extends Block {
+    public BronzeBlock(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        Item item = player.getStackInHand(Hand.MAIN_HAND).getItem();
+        if (item == Items.SHEARS) {
+            world.addBlockBreakParticles(pos, this.getDefaultState());
+            world.setBlockState(pos, BlockRegistry.CUT_BRONZE_BLOCK.getDefaultState());
+            return ActionResult.SUCCESS;
+        }
+        return ActionResult.PASS;
+    }
+}
