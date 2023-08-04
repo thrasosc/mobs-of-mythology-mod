eff33b92549f74dd27f6760bf5e7511f87f1770d
kyber-6 <100732009+kyber-6@users.noreply.github.com>
2023-08-04 21:55:13 +0300

    Changes

--- src/main/java/net/pixeldream/mythicmobs/registry/BlockEntityRegistry.java
+++ src/main/java/net/pixeldream/mythicmobs/registry/BlockEntityRegistry.java
@@ -1,14 +1,19 @@
 package net.pixeldream.mythicmobs.registry;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 import net.pixeldream.mythicmobs.MythicMobs;
 
 public class BlockEntityRegistry {
-//    public static BlockEntityType<RitualStoneBlockEntity> RITUAL_STONE_BLOCK_ENTITY;
+    public static BlockEntityType<RitualStoneBlockEntity> RITUAL_STONE_BLOCK_ENTITY;
 //    public static BlockEntityType<DrakeEggBlockEntity> DRAKE_EGG_BLOCK_ENTITY;
 //
-//    private static BlockEntity registerBlockEntity(String name, BlockEntity blockEntity) {
-//        return Registry.register(Registries.BLOCK_ENTITY_TYPE, new Identifier(MythicMobs.MOD_ID, name), blockEntity);
-//    }
+    private static BlockEntity registerBlockEntity(String name, BlockEntity blockEntity) {
+        return Registry.register(Registries.BLOCK_ENTITY_TYPE, new Identifier(MythicMobs.MOD_ID, name), new BlockEntity(blockEntity, new FabricItemSettings()));
+    }
 
     public static void initialize() {
         MythicMobs.LOGGER.info("Registering block entities for " + MythicMobs.MOD_NAME);
