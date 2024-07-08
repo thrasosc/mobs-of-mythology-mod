ce69190d70515ae9bc4b71636766d1893e9ab3c5
kyber-6 <100732009+kyber-6@users.noreply.github.com>
2024-07-08 13:29:07 +0300

    add neoforge client init

--- neoforge/src/main/java/net/pixeldreamstudios/mobs_of_mythology/neoforge/MobsOfMythologyNeoForge.java
+++ neoforge/src/main/java/net/pixeldreamstudios/mobs_of_mythology/neoforge/MobsOfMythologyNeoForge.java
@@ -1,30 +1,12 @@
 package net.pixeldreamstudios.mobs_of_mythology.neoforge;
 
-import net.neoforged.api.distmarker.Dist;
-import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.DistExecutor;
-import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
-
-import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.pixeldreamstudios.mobs_of_mythology.MobsOfMythology;
 
 @Mod(MobsOfMythology.MOD_ID)
 public final class MobsOfMythologyNeoForge {
     public MobsOfMythologyNeoForge() {
-        // Run our common setup.
         MobsOfMythology.init();
-        DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> MobsOfMythology::initClient);
     }
-
-//    @EventBusSubscriber(modid = MobsOfMythology.MOD_ID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-//    public static class ClientModEvents
-//    {
-//        @SubscribeEvent
-//        public static void onClientSetup(FMLClientSetupEvent event)
-//        {
-//            MobsOfMythology.initClient();
-//        }
-//    }
-
 }
+
