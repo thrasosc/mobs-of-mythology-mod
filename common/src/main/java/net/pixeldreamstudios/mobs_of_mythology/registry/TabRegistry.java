857a83ed64d9f3eef906fa52e0a416e96a405408
kyber-6 <100732009+kyber-6@users.noreply.github.com>
2024-07-10 21:13:05 +0300

    redefine creative tab

--- common/src/main/java/net/pixeldreamstudios/mobs_of_mythology/registry/TabRegistry.java
+++ common/src/main/java/net/pixeldreamstudios/mobs_of_mythology/registry/TabRegistry.java
@@ -11,8 +11,13 @@ import net.pixeldreamstudios.mobs_of_mythology.MobsOfMythology;
 
 public class TabRegistry {
     public static final DeferredRegister<CreativeModeTab> TABS = DeferredRegister.create(MobsOfMythology.MOD_ID, Registries.CREATIVE_MODE_TAB);
-    public static final RegistrySupplier<CreativeModeTab> MOBS_OF_MYTHOLOGY_TAB = TABS.register("mobs_of_mythology_tab", () ->
-            CreativeTabRegistry.create(Component.translatable("itemGroup." + MobsOfMythology.MOD_ID + ".mobs_of_mythology_tab"), () -> new ItemStack(ItemRegistry.GEAR.get())));
+    public static final RegistrySupplier<CreativeModeTab> MOBS_OF_MYTHOLOGY_TAB = TABS.register(
+            "mobs_of_mythology_tab", // Tab ID
+            () -> CreativeTabRegistry.create(
+                    Component.translatable("category.mobs_of_mythology"), // Tab Name
+                    () -> new ItemStack(ItemRegistry.GEAR.get()) // Icon
+            )
+    );
 
     public static void init() {
         TABS.register();
