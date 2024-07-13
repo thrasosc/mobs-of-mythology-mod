5b5e4e6b414567072ef5eecd8ca87f35cd606044
kyber-6 <100732009+kyber-6@users.noreply.github.com>
2024-07-13 16:22:17 +0300

    rework chupacabra

--- common/src/main/java/net/pixeldreamstudios/mobs_of_mythology/entity/monster/ChupacabraEntity.java
+++ common/src/main/java/net/pixeldreamstudios/mobs_of_mythology/entity/monster/ChupacabraEntity.java
@@ -7,12 +7,10 @@ import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.entity.ai.goal.*;
-import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
-import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.entity.monster.Enemy;
 import net.minecraft.world.entity.monster.Monster;
@@ -21,6 +19,21 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.pixeldreamstudios.mobs_of_mythology.MobsOfMythology;
 import net.tslat.smartbrainlib.api.SmartBrainOwner;
+import net.tslat.smartbrainlib.api.core.BrainActivityGroup;
+import net.tslat.smartbrainlib.api.core.behaviour.FirstApplicableBehaviour;
+import net.tslat.smartbrainlib.api.core.behaviour.OneRandomBehaviour;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.attack.AnimatableMeleeAttack;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.look.LookAtTarget;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.misc.Idle;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.move.FleeTarget;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.move.FloatToSurfaceOfFluid;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.move.MoveToWalkTarget;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.path.SetRandomWalkTarget;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.path.SetWalkTargetToAttackTarget;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.target.InvalidateAttackTarget;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.target.SetPlayerLookTarget;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.target.SetRandomLookTarget;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.target.TargetOrRetaliate;
 import net.tslat.smartbrainlib.api.core.navigation.SmoothGroundNavigation;
 import net.tslat.smartbrainlib.api.core.sensor.ExtendedSensor;
 import net.tslat.smartbrainlib.api.core.sensor.vanilla.HurtBySensor;
@@ -43,73 +56,74 @@ public class ChupacabraEntity extends AbstractMythMonsterEntity implements GeoEn
                 .add(Attributes.MOVEMENT_SPEED, 0.3);
     }
 
-    @Override
-    protected void registerGoals() {
-        this.goalSelector.addGoal(0, new FloatGoal(this));
-        this.goalSelector.addGoal(1, new MeleeAttackGoal(this, 1.5f, true));
-        this.goalSelector.addGoal(2, new WaterAvoidingRandomStrollGoal(this, 0.75f));
-        this.goalSelector.addGoal(3, new LookAtPlayerGoal(this, Player.class, 6.0f));
-        this.goalSelector.addGoal(4, new RandomLookAroundGoal(this));
-        this.targetSelector.addGoal(1, new HurtByTargetGoal(this));
-        this.targetSelector.addGoal(4, new NearestAttackableTargetGoal(this, Animal.class, false));
-        this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, Player.class, true));
-    }
+//    @Override
+//    protected void registerGoals() {
+//        this.goalSelector.addGoal(0, new FloatGoal(this));
+//        this.goalSelector.addGoal(1, new MeleeAttackGoal(this, 1.5f, true));
+//        this.goalSelector.addGoal(2, new WaterAvoidingRandomStrollGoal(this, 0.75f));
+//        this.goalSelector.addGoal(3, new LookAtPlayerGoal(this, Player.class, 6.0f));
+//        this.goalSelector.addGoal(4, new RandomLookAroundGoal(this));
+//        this.targetSelector.addGoal(1, new HurtByTargetGoal(this));
+//        this.targetSelector.addGoal(4, new NearestAttackableTargetGoal(this, Animal.class, false));
+//        this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, Player.class, true));
+//    }
 
     @Override
     public List<ExtendedSensor<ChupacabraEntity>> getSensors() {
         return ObjectArrayList.of(
                 new NearbyLivingEntitySensor<ChupacabraEntity>()
-                        .setPredicate((target, entity) -> target instanceof Player),
+                        .setPredicate((target, entity) -> target instanceof Player || target instanceof Animal),
                 new HurtBySensor<>()
         );
     }
 
     //TODO finish Chupcabra AI
-//    @Override
-//    public BrainActivityGroup<ChupacabraEntity> getCoreTasks() {
-//        return BrainActivityGroup.coreTasks(
-//                new FloatToSurfaceOfFluid<>(),
-//                new LookAtTarget(),
-//                new MoveToWalkTarget<>());
-//    }
-//
-//    @Override
-//    public BrainActivityGroup<ChupacabraEntity> getIdleTasks() { // These are the tasks that run when the mob isn't doing anything else (usually)
-//        return BrainActivityGroup.idleTasks(
-//                new FirstApplicableBehaviour<KoboldEntity>(      // Run only one of the below behaviours, trying each one in order. Include the generic type because JavaC is silly
-//                        new TargetOrRetaliate<>(),            // Set the attack target and walk target based on nearby entities
-//                        new SetPlayerLookTarget<>(),          // Set the look target for the nearest player
-//                        new SetRandomLookTarget<>()),         // Set a random look target
-//                new OneRandomBehaviour<>(                 // Run a random task from the below options
-//                        new SetRandomWalkTarget<>(),          // Set a random walk target to a nearby position
-//                        new Idle<>().runFor(entity -> entity.getRandom().nextInt(30, 60)))); // Do nothing for 1.5->3 seconds
-//    }
-//
-//    @Override
-//    public BrainActivityGroup<ChupacabraEntity> getFightTasks() { // These are the tasks that handle fighting
-//        return BrainActivityGroup.fightTasks(
-//                new InvalidateAttackTarget<>().invalidateIf((target, entity) -> !target.isAlive() || !entity.hasLineOfSight(target)),
-//                new SetWalkTargetToAttackTarget<>(),
-//                new AnimatableMeleeAttack<>(10),
-//                new FleeTarget<>()
-//        );
-//    }
-
     @Override
-    protected void customServerAiStep() {
-        tickBrain(this);
+    public BrainActivityGroup<ChupacabraEntity> getCoreTasks() {
+        return BrainActivityGroup.coreTasks(
+                new FloatToSurfaceOfFluid<>(),
+                new LookAtTarget(),
+                new MoveToWalkTarget<>());
     }
 
     @Override
-    public void tick() {
-        super.tick();
-        if (swinging) {
+    public BrainActivityGroup<ChupacabraEntity> getIdleTasks() {
+        return BrainActivityGroup.idleTasks(
+                new FirstApplicableBehaviour<KoboldEntity>(
+                        new TargetOrRetaliate<>(),
+                        new SetPlayerLookTarget<>(),
+                        new SetRandomLookTarget<>()),
+                new OneRandomBehaviour<>(
+                        new SetRandomWalkTarget<>(),
+                        new Idle<>().runFor(entity -> entity.getRandom().nextInt(30, 60))));
+    }
+
+    @Override
+    public BrainActivityGroup<ChupacabraEntity> getFightTasks() {
+        return BrainActivityGroup.fightTasks(
+                new InvalidateAttackTarget<>().invalidateIf((target, entity) -> !target.isAlive() || !entity.hasLineOfSight(target)),
+                new SetWalkTargetToAttackTarget<>(),
+                new AnimatableMeleeAttack<>(10),
+                new FleeTarget<>()
+        );
+    }
+
+    @Override
+    public boolean doHurtTarget(Entity entity) {
+        this.triggerAnim("attackController", "attack");
+        if (level().isClientSide()) {
             produceParticles(ParticleTypes.CRIMSON_SPORE);
             if (getHealth() < getMaxHealth()) {
                 this.heal(1.5f);
                 produceParticles(ParticleTypes.HAPPY_VILLAGER);
             }
         }
+        return super.doHurtTarget(entity);
+    }
+
+    @Override
+    protected void customServerAiStep() {
+        tickBrain(this);
     }
 
     @Override
