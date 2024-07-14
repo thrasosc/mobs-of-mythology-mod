1ee4086b0270fab8fabe99575560ca07d4a470e6
kyber-6 <100732009+kyber-6@users.noreply.github.com>
2024-07-14 20:35:35 +0300

    remove automaton spawning

--- common/src/main/java/net/pixeldreamstudios/mobs_of_mythology/registry/EntityRegistry.java
+++ common/src/main/java/net/pixeldreamstudios/mobs_of_mythology/registry/EntityRegistry.java
@@ -51,7 +51,6 @@ public class EntityRegistry {
                     .build(ResourceLocation.fromNamespaceAndPath(MobsOfMythology.MOD_ID, "sporeling").toString()));
 
     private static void initSpawns() {
-        BiomeModifications.addProperties(b -> b.hasTag(TagRegistry.HOT_BIOMES), (ctx, b) -> b.getSpawnProperties().addSpawn(MobCategory.CREATURE, new MobSpawnSettings.SpawnerData(AUTOMATON.get(), 5, 1, 1)));
         BiomeModifications.addProperties(b -> b.hasTag(TagRegistry.WET_BIOMES), (ctx, b) -> b.getSpawnProperties().addSpawn(MobCategory.MONSTER, new MobSpawnSettings.SpawnerData(KOBOLD.get(), 50, 1, 3)));
         BiomeModifications.addProperties(b -> b.hasTag(TagRegistry.WET_BIOMES), (ctx, b) -> b.getSpawnProperties().addSpawn(MobCategory.MONSTER, new MobSpawnSettings.SpawnerData(KOBOLD_WARRIOR.get(), 50, 1, 2)));
         BiomeModifications.addProperties(b -> b.hasTag(TagRegistry.DRY_BIOMES), (ctx, b) -> b.getSpawnProperties().addSpawn(MobCategory.MONSTER, new MobSpawnSettings.SpawnerData(DRAKE.get(), 10, 1, 1)));
