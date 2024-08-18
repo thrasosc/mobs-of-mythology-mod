6f53a55563a764bde182e9023e7cf11d5aa1222e
kyber-6 <100732009+kyber-6@users.noreply.github.com>
2024-08-18 13:22:53 +0300

    add dynamic saddle rendering

--- common/src/main/java/net/pixeldreamstudios/mobs_of_mythology/entity/client/renderer/BasiliskRenderer.java
+++ common/src/main/java/net/pixeldreamstudios/mobs_of_mythology/entity/client/renderer/BasiliskRenderer.java
@@ -1,10 +1,12 @@
 package net.pixeldreamstudios.mobs_of_mythology.entity.client.renderer;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.mojang.math.Axis;
 import mod.azure.azurelib.common.api.client.renderer.GeoEntityRenderer;
 import mod.azure.azurelib.common.api.client.renderer.layer.AutoGlowingGeoLayer;
 import mod.azure.azurelib.common.api.client.renderer.layer.BlockAndItemGeoLayer;
+import mod.azure.azurelib.common.internal.common.cache.object.BakedGeoModel;
 import mod.azure.azurelib.common.internal.common.cache.object.GeoBone;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
@@ -22,62 +24,47 @@ public class BasiliskRenderer extends GeoEntityRenderer<BasiliskEntity> {
         super(ctx, new BasiliskModel());
         this.shadowRadius = 0.75f;
         addRenderLayer(new AutoGlowingGeoLayer<>(this));
-
-        // Add some held item rendering
         addRenderLayer(new BlockAndItemGeoLayer<>(this) {
             @Nullable
             @Override
             protected ItemStack getStackForBone(GeoBone bone, BasiliskEntity animatable) {
-                // Retrieve the items in the entity's hands for the relevant bone
-
                 if (animatable.hasChest() && (bone.getName().equals("chest_left") || bone.getName().equals("chest_right"))) {
                     return new ItemStack(Items.CHEST);
                 }
                 return null;
-//                return switch (bone.getName()) {
-//                    case "chest_left", "chest_right" -> new ItemStack(Items.CHEST);
-//                    default -> null;
-//                };
             }
 
             @Override
             protected ItemDisplayContext getTransformTypeForStack(GeoBone bone, ItemStack stack, BasiliskEntity animatable) {
-                // Apply the camera transform for the given hand
                 if (animatable.hasChest()) {
                     return ItemDisplayContext.FIXED;
                 }
                 return ItemDisplayContext.NONE;
             }
 
-            // Do some quick render modifications depending on what the item is
             @Override
             protected void renderStackForBone(PoseStack poseStack, GeoBone bone, ItemStack stack, BasiliskEntity animatable, MultiBufferSource bufferSource, float partialTick, int packedLight, int packedOverlay) {
-
                 if (bone.getName().equals("chest_left")) {
                     poseStack.mulPose(Axis.YP.rotationDegrees(90));
                 } else if (bone.getName().equals("chest_right")) {
                     poseStack.mulPose(Axis.YP.rotationDegrees(-90));
                 }
-//                if (stack == BasiliskRenderer.this.mainHandItem) {
-//                    poseStack.mulPose(Axis.XP.rotationDegrees(-90f));
-//
-//                    if (stack.getItem() instanceof ShieldItem)
-//                        poseStack.translate(0, 0.125, -0.25);
-//                } else if (stack == KoboldRenderer.this.offHandItem) {
-//                    poseStack.mulPose(Axis.XP.rotationDegrees(-90f));
-//
-//                    if (stack.getItem() instanceof ShieldItem) {
-//                        poseStack.translate(0, 0.125, 0.25);
-//                        poseStack.mulPose(Axis.YP.rotationDegrees(180));
-//                    }
-//                }
-
                 super.renderStackForBone(poseStack, bone, stack, animatable, bufferSource, partialTick, packedLight, packedOverlay);
             }
         });
     }
 
     @Override
+    public void preRender(PoseStack poseStack, BasiliskEntity animatable, BakedGeoModel model, MultiBufferSource bufferSource, VertexConsumer buffer, boolean isReRender, float partialTick, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
+        super.preRender(poseStack, animatable, model, bufferSource, buffer, isReRender, partialTick, packedLight, packedOverlay, red, green, blue, alpha);
+        if (animatable.isSaddled()) {
+            model.getBone("saddle").get().setHidden(false);
+        } else {
+            model.getBone("saddle").get().setHidden(true);
+        }
+    }
+
+        @Override
     public ResourceLocation getTextureLocation(BasiliskEntity animatable) {
         return ResourceLocation.fromNamespaceAndPath(MobsOfMythology.MOD_ID, "textures/entity/basilisk.png");
     }
