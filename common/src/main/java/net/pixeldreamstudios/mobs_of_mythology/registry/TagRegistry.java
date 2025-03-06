1bb688d33f0d15e5ace08b85f455fecb04e637e0
Thrasos <100732009+thrasosc@users.noreply.github.com>
2025-03-06 20:11:09 +0100

    fix tags

--- common/src/main/java/net/pixeldreamstudios/mobs_of_mythology/registry/TagRegistry.java
+++ common/src/main/java/net/pixeldreamstudios/mobs_of_mythology/registry/TagRegistry.java
@@ -10,20 +10,15 @@ import net.minecraft.world.level.block.Block;
 
 public record TagRegistry() {
     // BLOCK TAGS
-    public static final TagKey<Block> BRONZE_BLOCKS = TagKey.create(Registries.BLOCK, new ResourceLocation("c", "bronze_blocks"));
+    public static final TagKey<Block> BRONZE_BLOCKS = TagKey.create(Registries.BLOCK, Platform.isForge() ? new ResourceLocation("forge", "storage_blocks/bronze") : new ResourceLocation("c", "bronze_blocks"));
 
     // ITEM TAGS
-    public static final TagKey<Item> PICKAXES = TagKey.create(Registries.ITEM, new ResourceLocation("c", "pickaxes"));
-    public static final TagKey<Item> BRONZE_INGOTS = TagKey.create(Registries.ITEM, new ResourceLocation("c", "bronze_ingots"));
+    public static final TagKey<Item> PICKAXES = TagKey.create(Registries.ITEM, Platform.isForge() ? new ResourceLocation("forge", "pickaxes") : new ResourceLocation("c", "pickaxes"));
+    public static final TagKey<Item> BRONZE_INGOTS = TagKey.create(Registries.ITEM, Platform.isForge() ? new ResourceLocation("forge", "ingots/bronze") : new ResourceLocation("c", "bronze_ingots"));
 
     // BIOME TAGS
-    public static TagKey<Biome> WET_BIOMES = TagKey.create(Registries.BIOME, Platform.isForge() ?  new ResourceLocation("forge", "is_wet")
-            : new ResourceLocation("c", "climate_wet"));
-    public static TagKey<Biome> BADLANDS_BIOMES = TagKey.create(Registries.BIOME,Platform.isForge() ?  new ResourceLocation("forge", "is_dry")
-            :  new ResourceLocation("c", "badlands"));
-    public static TagKey<Biome> MUSHROOM_BIOMES = TagKey.create(Registries.BIOME,Platform.isForge() ?  new ResourceLocation("forge", "is_mushroom")
-            :  new ResourceLocation("c", "mushroom"));
-    public static TagKey<Biome> TEMPERATE_BIOMES = TagKey.create(Registries.BIOME,Platform.isForge() ?  new ResourceLocation("forge", "is_hot")
-            :  new ResourceLocation("c", "climate_temperate"));
-
+    public static TagKey<Biome> WET_BIOMES = TagKey.create(Registries.BIOME, Platform.isForge() ? new ResourceLocation("forge", "is_wet") : new ResourceLocation("c", "climate_wet"));
+    public static TagKey<Biome> BADLANDS_BIOMES = TagKey.create(Registries.BIOME, Platform.isForge() ? new ResourceLocation("forge", "is_dry") : new ResourceLocation("c", "badlands"));
+    public static TagKey<Biome> MUSHROOM_BIOMES = TagKey.create(Registries.BIOME, Platform.isForge() ? new ResourceLocation("forge", "is_mushroom") : new ResourceLocation("c", "mushroom"));
+    public static TagKey<Biome> TEMPERATE_BIOMES = TagKey.create(Registries.BIOME, Platform.isForge() ? new ResourceLocation("forge", "is_hot") : new ResourceLocation("c", "climate_temperate"));
 }
