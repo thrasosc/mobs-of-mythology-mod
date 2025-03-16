6f5f6a07b4c6e8e3b748b57dfe189297cb28b8b9
thrasosc <100732009+thrasosc@users.noreply.github.com>
2025-03-16 16:13:41 +0100

    use mod id

--- common/src/main/java/net/pixeldreamstudios/mobs_of_mythology/registry/TagRegistry.java
+++ common/src/main/java/net/pixeldreamstudios/mobs_of_mythology/registry/TagRegistry.java
@@ -6,6 +6,7 @@ import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.block.Block;
+import net.pixeldreamstudios.mobs_of_mythology.MobsOfMythology;
 
 public record TagRegistry() {
     // BLOCK TAGS
@@ -16,9 +17,9 @@ public record TagRegistry() {
     public static final TagKey<Item> BRONZE_INGOTS = TagKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath("c", "bronze_ingots"));
 
     // MOB BIOME SPAWN TAGS
-    public static TagKey<Biome> KOBOLD_BIOMES = TagKey.create(Registries.BIOME, ResourceLocation.fromNamespaceAndPath("mobs_of_mythology", "kobolds_spawn_in"));
-    public static TagKey<Biome> PEGASUS_BIOMES = TagKey.create(Registries.BIOME, ResourceLocation.fromNamespaceAndPath("mobs_of_mythology", "pegasus_spawn_in"));
-    public static TagKey<Biome> DRAKE_BIOMES = TagKey.create(Registries.BIOME, ResourceLocation.fromNamespaceAndPath("mobs_of_mythology", "drakes_spawn_in"));
-    public static TagKey<Biome> CHUPACABRA_BIOMES = TagKey.create(Registries.BIOME, ResourceLocation.fromNamespaceAndPath("mobs_of_mythology", "chupacabras_spawn_in"));
-    public static TagKey<Biome> SPORELING_BIOMES = TagKey.create(Registries.BIOME, ResourceLocation.fromNamespaceAndPath("mobs_of_mythology", "sporelings_spawn_in"));
+    public static TagKey<Biome> KOBOLD_BIOMES = TagKey.create(Registries.BIOME, ResourceLocation.fromNamespaceAndPath(MobsOfMythology.MOD_ID, "kobolds_spawn_in"));
+    public static TagKey<Biome> PEGASUS_BIOMES = TagKey.create(Registries.BIOME, ResourceLocation.fromNamespaceAndPath(MobsOfMythology.MOD_ID, "pegasus_spawn_in"));
+    public static TagKey<Biome> DRAKE_BIOMES = TagKey.create(Registries.BIOME, ResourceLocation.fromNamespaceAndPath(MobsOfMythology.MOD_ID, "drakes_spawn_in"));
+    public static TagKey<Biome> CHUPACABRA_BIOMES = TagKey.create(Registries.BIOME, ResourceLocation.fromNamespaceAndPath(MobsOfMythology.MOD_ID, "chupacabras_spawn_in"));
+    public static TagKey<Biome> SPORELING_BIOMES = TagKey.create(Registries.BIOME, ResourceLocation.fromNamespaceAndPath(MobsOfMythology.MOD_ID, "sporelings_spawn_in"));
 }
